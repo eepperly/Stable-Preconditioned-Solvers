327e95c78661e6761b1baf343f5baf360540228d
Ethan Epperly <eepperly@caltech.edu>
2025-02-08 15:40:14 -0800

    Add automatic version of LSQR-IR

--- code/lsqrir.m
+++ code/lsqrir.m
@@ -1,13 +1,89 @@
-function [x,stats] = lsqrir(matvec,adjvec,b,tol,iter,varargin)
+function [y,stats] = lsqrir(Afun,Atfun,Pfun,Ptfun,b,tol,iterschedule,varargin)
     summary = [];
-    if ~isempty(varargin); summary = varargin{1}; end
+    if ~isempty(varargin)
+        summary = varargin{1};
+    end
+    if length(varargin) > 1 && ~isempty(varargin{2})
+        verbose = varargin{2};
+    else
+        verbose = false;
+    end
+
+    y = [];
+
     stats = [];
-    x = zeros(size(adjvec(zeros(size(b,1),0)),1));
-    for i = 1:length(iter)
-        if i > 1 && ~isempty(stats)
-            stats(end,:) = [];
+    matvec = @(y) Afun(Pfun(y));
+    adjvec = @(y) Ptfun(Atfun(y));
+
+    oldberr = [];
+    if length(iterschedule) == 1 && tol > 0
+        assert(tol > 0)
+        stagnation_tol = 0.9;
+        stagnation_check_frequency = iterschedule;
+        iterschedule = 100*ones(50,1);
+    else
+        stagnation_tol = Inf;
+        stagnation_check_frequency = Inf;
+    end
+    found_solution = false;
+
+    if tol > 0
+        n = length(b);
+        x = randn(n,1);
+        for i = 1:ceil(log(n))
+            x = Atfun(Afun(x)); x = x / norm(x);
         end
-        [x,~,newstats] = mylsqr(matvec,adjvec,b,tol,iter(i),summary,x);
-        stats = [stats;newstats];
+        Anorm = norm(Atfun(x));
+    end
+
+    beta = norm(b); u = b / beta;
+    for i = 1:length(iterschedule)
+        v = adjvec(u); alpha = norm(v); v = v / alpha;
+        if isempty(y); y = zeros(size(v)); end
+        w = v;
+        phibar = beta; rhobar = alpha;
+        resnorm = beta * alpha;
+        if ~isempty(summary) && i == 1; stats(end+1,:) = summary(y); end 
+        numiters = iterschedule(i);
+        for iter = 1:numiters
+            u = matvec(v) - alpha*u; beta = norm(u); u = u / beta;
+            v = adjvec(u) - beta*v; alpha = norm(v); v = v / alpha;
+            rho = sqrt(rhobar^2 + beta^2);
+            c = rhobar / rho;
+            s = beta / rho;
+            theta = s * alpha;
+            rhobar = - c * alpha;
+            phi = c * phibar;
+            phibar = s * phibar;
+            y = y + (phi/rho) * w;
+            w = v - (theta/rho) * w;
+            if ~isempty(summary); stats(end+1,:) = summary(y); end %#ok<AGROW> 
+            if verbose
+                fprintf('%d\t%e\n',iter,abs(phibar * alpha * c))
+            end
+
+            if mod(iter,stagnation_check_frequency) == 0 && tol > 0
+                x = Pfun(y);
+                berr = norm(b - Afun(x)) / (norm(x) * Anorm);
+
+                if berr <= tol
+                    found_solution = true;
+                    break; 
+                end
+
+                if ~isempty(oldberr) && berr > stagnation_tol * oldberr
+                    if verbose
+                        fprintf("Restarting at %d/%d: %e/%e\n",iter,i,berr,oldberr)
+                    end
+                    oldberr = berr;
+                    break;
+                end
+
+                oldberr = berr;
+            end
+        end
+
+        if found_solution; break; end    
+        u = b - matvec(y); beta = norm(u); u = u / beta;
     end
 end
--- paper3_experiments/test_lsqr.m
+++ paper3_experiments/test_lsqr.m
@@ -14,19 +14,20 @@ x = randn(n,1); b = A*x;
 cond(A*Pinv)
 normA = norm(A);
 normx = norm(x);
-summary = @(y) [norm(b-A*(Pinv*y))/(normA*normx) norm(x-Pinv*y)/normx];
+summary = @(y) [norm(b-A*(Pinv*y))/(normA*normx) norm(x-Pinv*y)/normx norm(b-A*(Pinv*y))/(normA*norm(Pinv*y))];
+niter = 100;
 
-[~,~,lsqr_res] = mylsqr(@(y) A*(Pinv*y), @(y) Pinv'*(A'*y),b,0,100,summary);
-[~,lsqrir_res] = lsqrir(@(y) A*(Pinv*y), @(y) Pinv'*(A'*y),b,0,[50,50],summary);
-[~,gmres_res] = mygmres(@(y) A*(Pinv*y),b,100,summary);
+[~,~,lsqr_res] = mylsqr(@(y) A*(Pinv*y), @(y) Pinv'*(A'*y),b,0,niter,summary,[],[]);
+[~,lsqrir_res] = lsqrir(@(y) A*y, @(y) A'*y, @(y) Pinv*y, @(y) Pinv'*y, b, 0, [50,50], summary);
+[~,gmres_res] = mygmres(@(y) A*(Pinv*y),b,niter,summary);
 
 close all
 figure
-semilogy(0:100,lsqr_res(:,1),"Color",blue,"LineWidth",3); hold on
-semilogy(0:100,lsqrir_res(:,1),"--","Color",orange,"LineWidth",3)
-semilogy(0:100,gmres_res(:,1),"-.","Color",pink,"LineWidth",3)
+semilogy(0:niter,lsqr_res(:,1),"Color",blue,"LineWidth",3); hold on
+semilogy(0:(length(lsqrir_res(:,1))-1),lsqrir_res(:,1),"--","Color",orange,"LineWidth",3)
+semilogy(0:niter,gmres_res(:,1),"-.","Color",pink,"LineWidth",3)
 yline(norm(b-A*(A\b))/(normA*normx),":","Color",black,"LineWidth",3)
-axis([0 100 1e-17 1e0])
+axis([0 niter 1e-17 1e0])
 legend({"PLSQR","PLSQR-IR","PGMRES","Direct"},"Location","best")
 xlabel("Iteration $i$")
 ylabel("Residual $\|\mbox{\boldmath $b$}-\mbox{\boldmath $A$}\mbox{\boldmath $x$}_i\| / \|\mbox{\boldmath $A$}\| \|\mbox{\boldmath $x$}\|$")
@@ -34,11 +35,22 @@ exportgraphics(gcf,"../figs/lsqr_backward.png")
 saveas(gcf,"../figs/lsqr_backward.fig")
 
 figure
-semilogy(0:100,lsqr_res(:,2),"Color",blue,"LineWidth",3); hold on
-semilogy(0:100,lsqrir_res(:,2),"--","Color",orange,"LineWidth",3)
-semilogy(0:100,gmres_res(:,2),"-.","Color",pink,"LineWidth",3)
+semilogy(0:niter,lsqr_res(:,2),"Color",blue,"LineWidth",3); hold on
+semilogy(0:(length(lsqrir_res(:,2))-1),lsqrir_res(:,2),"--","Color",orange,"LineWidth",3)
+semilogy(0:niter,gmres_res(:,2),"-.","Color",pink,"LineWidth",3)
 yline(norm(x-(A\b))/(normx),":","Color",black,"LineWidth",3)
+axis([0 niter -Inf Inf])
 xlabel("Iteration $i$")
 ylabel("Forward error $\|\mbox{\boldmath $x$}-\mbox{\boldmath $x$}_i\| / \|\mbox{\boldmath $x$}\|$")
 exportgraphics(gcf,"../figs/lsqr_forward.png")
 saveas(gcf,"../figs/lsqr_forward.fig")
+
+figure
+semilogy(0:niter,lsqr_res(:,3),"Color",blue,"LineWidth",3); hold on
+semilogy(0:(length(lsqrir_res(:,3))-1),lsqrir_res(:,3),"--","Color",orange,"LineWidth",3)
+semilogy(0:niter,gmres_res(:,3),"-.","Color",pink,"LineWidth",3)
+yline(norm(b-A*(A\b))/(normA*norm(A\b)),":","Color",black,"LineWidth",3)
+xlabel("Iteration $i$")
+ylabel("Backward error $\|\mbox{\boldmath $b$}-\mbox{\boldmath $A$}\mbox{\boldmath $x$}_i\| / \|\mbox{\boldmath $A$}\| \|\mbox{\boldmath $x$}_i\|$")
+exportgraphics(gcf,"../figs/lsqr_backward_2.png")
+saveas(gcf,"../figs/lsqr_backward_2.fig")
